5bdee0ae5add048a065d27878d67d5a59d5b7723
Tanvir Hassan Anik <tanvir.h.anik@gmail.com>
2022-11-02 23:45:54 +0600

    .

--- lib/pages/play_pads_page.dart
+++ lib/pages/play_pads_page.dart
@@ -76,10 +76,7 @@ class _PlayPadsPageState extends State<PlayPadsPage> {
         gradient: const LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
-            colors: <Color>[
-              Color(0xff02aab0),
-              Color(0xff00cdac),
-            ]),
+            colors: <Color>[Color(0xff02aab0), Color(0xff00cdac)]),
         color: Colors.blue,
         borderRadius: BorderRadius.circular(12),
       ),
@@ -90,13 +87,9 @@ class _PlayPadsPageState extends State<PlayPadsPage> {
             padding: const EdgeInsets.all(8.0),
             child: Row(
               children: [
-                SizedBox(
-                  child: Image.asset('lib/assets/icon/icon.png', fit: BoxFit.cover),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
+                SizedBox(child: Image.asset('lib/assets/icon/icon.png', fit: BoxFit.cover)),
+                const SizedBox(width: 10),
+                const Text(
                   "Play",
                   style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
                 ),
@@ -106,13 +99,8 @@ class _PlayPadsPageState extends State<PlayPadsPage> {
           Row(
             children: [
               IconButton(
-                icon: const Icon(
-                  Icons.sync,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  setState(() {});
-                },
+                icon: const Icon(Icons.sync, color: Colors.white),
+                onPressed: () => setState(() {}),
               ),
               IconButton(
                 icon: const Icon(
